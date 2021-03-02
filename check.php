d7e968d331b6e9bc255f6657d8fbcefe6d267893
Yuto <>
2021-03-02 12:21:13 +0900

    first commit

---
+++ check.php
@@ -0,0 +1,56 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        header('Location: index.htm');
+    }
+
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $content = $_POST['content'];
+
+    if ($nickname == '') {
+        $nickname_result = 'ニックネームが入力されていません。';
+    } else {
+        $nickname_result = 'ようこそ、' . $nickname .'様';
+    }
+    
+    if ($email == '') {
+        $email_result = 'メールアドレスが入力されていません。';
+    } else {
+        $email_result = 'メールアドレス：' . $email;
+    }
+
+    if ($content == '') {
+        $content_result =  'お問い合わせ内容が入力されていません。';
+    } else {
+        $content_result = 'お問い合わせ内容：' . $content;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>入力内容確認</title>
+</head>
+<body>
+    <h1>入力内容確認</h1>
+    <p><?php echo $nickname_result; ?></p>
+    <p><?php echo $email_result; ?></p>
+    <p><?php echo $content_result; ?></p>
+
+    <form method="POST" action="thanks.php">
+        <!-- thanks.phpへデータを渡すために formデータじゃないとエラーが起きる -->
+        <input type="hidden" name="nickname" value="<?php echo $nickname; ?>">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <input type="hidden" name="content" value="<?php echo $content; ?>">
+
+        <button type="button" onclick="history.back()">戻る</button>
+        <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+        <button type="submit">OK</button>
+        <?php endif; ?>
+    </form>
+    
+</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,25 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        header('Location: index.htm');
+    }
+
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $content = $_POST['content'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>送信完了</title>
+</head>
+<body>
+    <h1>お問い合わせありがとうございました！</h1>
+    <p><?php echo $nickname; ?></p>
+    <p><?php echo $email; ?></p>
+    <p><?php echo $content; ?></p>
+</body>
+</html>
